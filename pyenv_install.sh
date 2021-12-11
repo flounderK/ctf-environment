2d31c7a08eca5d959cd89c891ad298707187108c
Clif Wolfe <clifwolfe1@gmail.com>
2021-12-11 14:41:02 -0500

    Added pyenv install script

---
+++ pyenv_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+curl https://pyenv.run | bash
+
+GLOBAL_VERSION="3.7.0"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv virtualenv-init -)"
+
+RCFILE="$HOME/.bashrc"
+
+BUILD_DEPS_URL=https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+
+echo "Fetching build deps"
+UBUNTU_BUILD_DEPS_STR=$(curl "$BUILD_DEPS_URL" | grep --color=never -Poz '(?<=copy-content=")sudo apt[^"]*' | tr -d '\n' | tr -d '\\' | sed 's/ install / install -y /g')
+
+echo "\nRunning '$UBUNTU_BUILD_DEPS_STR'\n"
+eval "$UBUNTU_BUILD_DEPS_STR"
+
+pyenv install "$GLOBAL_VERSION"
+pyenv global "$GLOBAL_VERSION"
+
+
+# the sed invocation inserts the lines at the start of the file
+# after any initial comment lines
+sed -Ei -e '/^([^#]|$)/ {a \
+export PYENV_ROOT="$HOME/.pyenv"
+a \
+export PATH="$PYENV_ROOT/bin:$PATH"
+a \
+' -e ':a' -e '$!{n;ba};}' ~/.profile
+echo 'eval "$(pyenv init --path)"' >>~/.profile
+
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+
+echo 'pyenv will be fully initialized on next login. Until then, you can run these lines in any terminal that you create'
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' 
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' 
+echo 'eval "$(pyenv init --path)"'
+echo 'eval "$(pyenv init -)"' 
