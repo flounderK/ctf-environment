a588d53f1616b1d3b626111619cb574411376916
Clif Wolfe <clifwolfe1@gmail.com>
2021-12-12 17:19:46 -0500

    pyenv install mostly works

---
+++ roles/pyenv/files/bash_profile_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sed -Ei -e '/^([^#]|$)/ {a \
+export PYENV_ROOT="$HOME/.pyenv"
+a \
+export PATH="$PYENV_ROOT/bin:$PATH"
+a \
+' -e ':a' -e '$!{n;ba};}' ~/.profile
+echo 'eval "$(pyenv init --path)"' >>~/.profile
+
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
