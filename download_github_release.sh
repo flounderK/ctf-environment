d33fb84f40f7227aea0b5b80a4f75c58b7c18783
Clif Wolfe <clifwolfe1@gmail.com>
2021-12-23 11:50:07 -0500

    remove old broken github release downloader

--- download_github_release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$#" -lt 1 ]; then
-	echo "Usage: $0 'owner/repo'"
-	exit 1
-fi
-URL=$(curl "https://api.github.com/repos/$1/releases/latest" | grep --color=never -Po '(?<=browser_download_url": ")[^"]+')
-wget "$URL" 
-echo $(basename "$URL")
