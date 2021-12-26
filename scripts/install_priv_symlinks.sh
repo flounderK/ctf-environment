0982ed0325bb3b07b54feb033572bdae92490822
Clif Wolfe <clifwolfe1@gmail.com>
2021-12-26 17:49:33 -0500

    Priv symlinks now selects the correct directory based off of when it was last modified

--- scripts/install_priv_symlinks.sh
+++ scripts/install_priv_symlinks.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo ln -f -s /opt/upx*/ /opt/upx
-sudo ln -f -s /opt/ghidra*/ /opt/ghidra
+INSTALL_DIR="/opt"
+SORTED_DIRS=$(find "$INSTALL_DIR" -maxdepth 1 -mindepth 1 -type d -exec ls -1dt "{}" \;)
+
+link_last_modified() {
+	# 1 is match
+	# 2 is linkname
+	TARGET=$(echo "$SORTED_DIRS" | grep --color=never -i "$1" | head -n1)
+	LINK="$INSTALL_DIR/$2"
+	echo "target $TARGET"
+	echo "link $LINK"
+	sudo ln -f -s "$TARGET" "$LINK"
+}
+
+link_last_modified "upx" "upx"
+link_last_modified "ghidra" "ghidra"
+link_last_modified "cutter" "cutter"
