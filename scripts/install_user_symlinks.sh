66cf1dfe364445627b222d5cca6cc29c4842e0e0
Clif Wolfe <clifwolfe1@gmail.com>
2022-01-02 21:07:53 -0500

    Added codeql binariesto github installs

--- scripts/install_user_symlinks.sh
+++ scripts/install_user_symlinks.sh
@@ -5,4 +5,5 @@ mkdir -p "$HOME/.local/bin"
 ln -f -s /opt/ghidra/ghidraRun "$HOME/.local/bin/ghidra"
 ln -f -s /opt/upx/upx "$HOME/.local/bin/upx"
 ln -f -s /opt/cutter/*.AppImage "$HOME/.local/bin/cutter"
+ln -f -s /opt/codeql/codeql "$HOME/.local/bin/codeql"
 
