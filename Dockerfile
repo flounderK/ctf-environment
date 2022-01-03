5361be1484edfc3cdf051729fceeebd270fd6716
Clif Wolfe <clifwolfe1@gmail.com>
2022-01-03 16:19:51 -0500

    Pyenv install caching for docker compose

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV TZ=America/New_York
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # RUN echo 'Acquire::http { Proxy "http://dockerhost:3142"; };' >> /etc/apt/apt.conf.d/01proxy
 RUN apt update && apt upgrade -y
-RUN apt install -y git sudo curl
+RUN apt install -y git sudo curl sed
 
 RUN mkdir -p /workdir/ctf-environment
 WORKDIR /workdir/ctf-environment
@@ -15,6 +15,8 @@ RUN echo "ALL       ALL = (ALL) NOPASSWD: ALL" >> /etc/sudoers
 RUN useradd -m -s "/bin/bash" --uid=1000 testuser
 
 RUN chown -R testuser:testuser /workdir/ctf-environment
+RUN mkdir -p /pyenv_versions_cache
+RUN chown -R testuser:testuser /pyenv_versions_cache
 
 USER 1000:1000
 
--- scripts/pyenv_install.sh
+++ scripts/pyenv_install.sh
@@ -5,7 +5,20 @@ curl https://pyenv.run | bash
 GLOBAL_VERSION="3.8.10"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+echo "$PATH"
 eval "$(pyenv virtualenv-init -)"
+PYENV_VERSIONS_DIR="$PYENV_ROOT/versions"
+
+if [[ -f /.dockerenv && -d /pyenv_versions_cache ]]; then
+	echo "In docker container and pyenv versions cache found"
+	mkdir -p "$PYENV_VERSIONS_DIR"
+	for i in $(find /pyenv_versions_cache -maxdepth 1 -mindepth 1 -type d); do
+		# ln -f -s "$PYENV_VERSIONS_DIR/$(basename $i)" "$i"
+		if [ ! -d "$PYENV_VERSIONS_DIR/$(basename $i)" ]; then
+			cp -r "$i" "$PYENV_VERSIONS_DIR/$(basename $i)"
+		fi
+	done
+fi
 
 RCFILE="$HOME/.bashrc"
 
@@ -25,6 +38,14 @@ do echo "Pyenv install failed"
 done
 pyenv global "$GLOBAL_VERSION"
 
+if [[ -f /.dockerenv && -d /pyenv_versions_cache ]]; then
+	for i in $(find "$PYENV_VERSIONS_DIR" -maxdepth 1 -mindepth 1 -type d); do
+		if [ ! -d "/pyenv_versions_cache/$(basename $i)" ]; then
+			cp -r "$i" /pyenv_versions_cache
+		fi
+	done
+fi
+
 
 # the sed invocation inserts the lines at the start of the file
 # after any initial comment lines
@@ -34,11 +55,11 @@ a \
 export PATH="$PYENV_ROOT/bin:$PATH"
 a \
 ' -e ':a' -e '$!{n;ba};}' ~/.profile
-echo 'eval "$(pyenv init --path)"' >>~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
 
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
-source "$HOME/.profile"
+source "$HOME/.profile" && source "$HOME/.bashrc"
 
 echo 'Updating pip'
 pip install -U pip
